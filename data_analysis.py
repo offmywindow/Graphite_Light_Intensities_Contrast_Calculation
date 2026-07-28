f8273802cbc487b1ba249e349e2b89cbc7578829
offmywindow <83930378+offmywindow@users.noreply.github.com>
2026-07-27 18:13:47 -0700

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -18,15 +18,10 @@ class arwfile:
         with r.imread(self.file_path) as raw:
             self.raw_data = raw.raw_image.copy().astype(np.float32)
             self.corrected_data = np.maximum(self.raw_data - black_level, 0)
-            # print(raw.pattern) confirmed the number to be pick up from each
-            self.R = self.corrected_data[0::2, 0::2]
-            self.G1 = self.corrected_data[0::2, 1::2]
-            self.G2 = self.corrected_data[1::2, 0::2]
-            self.B = self.corrected_data[1::2, 1::2]
             self.rgb_image = raw.postprocess()
 
     # Function to analysis data from rois,collect mean values from each channel
-    def mean_analysis(self, rois):
+    def mean_analysis(self, rois,method):
 
         for i, roi in enumerate(rois):
             x1, y1, x2, y2 = roi
@@ -39,18 +34,26 @@ class arwfile:
             error generated because of rounding """
             for x, y in zip(x_values, y_values):
                 value = self.corrected_data[y, x]  # the light intensity value
+                # print(raw.pattern) confirmed the number to be pick up from each
                 if x % 2 == 0 and y % 2 == 0:
                     self.intensities["red"][i].append(value)
                 elif x % 2 == 1 and y % 2 == 1:
                     self.intensities["blue"][i].append(value)
                 else:
                     self.intensities["green"][i].append(value)
-
-            self.mean_red.append(np.mean(self.intensities["red"][i]))
-            self.mean_blue.append(np.mean(self.intensities["blue"][i]))
-            self.mean_green.append(np.mean(self.intensities["green"][i]))
+            if method == "Full Mean Contrast Calculation":
+                self.mean_red.append(np.mean(self.intensities["red"][i]))
+                self.mean_blue.append(np.mean(self.intensities["blue"][i]))
+                self.mean_green.append(np.mean(self.intensities["green"][i]))
+            elif method == "Mid 80% Intensity Contrast Calculation":
+                for channel in self.intensities:
+                    for x in self.intensities[channel]:
+                        
+            
 
     def contrast_calculation(self):
         self.red_contrast = (self.mean_red[0] - self.mean_red[1]) / self.mean_red[1]
         self.blue_contrast = (self.mean_blue[0] - self.mean_blue[1]) / self.mean_blue[1]
         self.green_contrast = (self.mean_green[0] - self.mean_green[1]) / self.mean_green[1]
+
+
