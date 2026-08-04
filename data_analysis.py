cfd76f30dacc7fb585b466cf87944626ca0cf321
offmywindow <83930378+offmywindow@users.noreply.github.com>
2026-08-03 17:43:14 -0700

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,6 @@
 import rawpy as r
 import numpy as np
-from PIL import Image,ImageDraw
+from PIL import Image, ImageDraw
 
 # black level is a constant,512
 # black_level = raw.black_level_per_channel
@@ -22,7 +22,7 @@ class arwfile:
             self.rgb_image = raw.postprocess()
 
     # Function to analysis data from rois,collect mean values from each channel
-    def line_analysis(self, rois,method):
+    def line_analysis(self, rois, method):
 
         for i, roi in enumerate(rois):
             x1, y1, x2, y2 = roi
@@ -32,7 +32,7 @@ class arwfile:
             y_values = np.round(np.linspace(y1, y2, length)).astype(int)
 
             """Extract green, red and blue from each photosite,but there's maybe some
-            error generated because of rounding """
+            error generated because of rounding"""
             for x, y in zip(x_values, y_values):
                 value = self.corrected_data[y, x]  # the light intensity value
                 # print(raw.pattern) confirmed the number to be pick up from each
@@ -43,43 +43,76 @@ class arwfile:
                 else:
                     self.intensities["green"][i].append(value)
 
-            #Two methods of mean_contrast calculation
+            # Two methods of mean_contrast calculation
             if method == "Full Mean Contrast Calculation":
                 pass
             elif method == "Mid 80% Intensity Contrast Calculation":
                 for channel in self.intensities:
-                    #transfer the list to numpy array
+                    # transfer the list to numpy array
                     values = np.array(self.intensities[channel][i])
 
-                    low = np.percentile(values,10)
+                    low = np.percentile(values, 10)
                     high = np.percentile(values, 90)
-                    mid_80 = values[(values>low)&(values<high)]
-                    #The reason convert numpy array to python list is make sure the function "cleareverything"(.clear()method) in interface moduel works fine
+                    mid_80 = values[(values > low) & (values < high)]
+                    # The reason convert numpy array to python list is make sure the function "cleareverything"(.clear()method) in interface moduel works fine
                     self.intensities[channel][i] = mid_80.tolist()
 
         self.mean_analysis()
 
-    #mean intensity analysis when roi pick method is rectangle
-    def polygon_analysis(self, rois,method):
-        self.polygon_points = []
-        for point_sets in rois:
-            for points in point_sets:
-                
-            height,width = self.raw_data.shape
-            mask = Image.new("L",(width,height),0)
+    # mean intensity analysis when roi pick method is rectangle
+    def polygon_analysis(self, rois, method):
 
+        height, width = self.raw_data.shape
+
+        for i,points in enumerate(rois):
+
+            mask = Image.new("L", (width, height), 0)
             draw = ImageDraw.Draw(mask)
-            draw.polygon()
-    #function to analysis mean intensities before calculating contrast
+            draw.polygon(points, outline=1, fill=1)
+            # turn in the picture into a numpy array
+            mask_array = np.array(mask)
+
+            #get all the coordinates from mask_array, all the coordinates inside the polygon
+            #numpy.nonzero() function returns the indices of all non-zero (or True) elements within an array.
+            y_values,x_values = np.nonzero(mask_array)
+
+            for y,x in zip(y_values,x_values):
+
+                value = self.corrected_data[y, x]  # the light intensity value
+                # print(raw.pattern) confirmed the number to be pick up from each
+                if x % 2 == 0 and y % 2 == 0:
+                    self.intensities["red"][i].append(value)
+                elif x % 2 == 1 and y % 2 == 1:
+                    self.intensities["blue"][i].append(value)
+                else:
+                    self.intensities["green"][i].append(value)
+
+            # Two methods of mean_contrast calculation
+            if method == "Full Mean Contrast Calculation":
+                pass
+            elif method == "Mid 80% Intensity Contrast Calculation":
+                for channel in self.intensities:
+                    # transfer the list to numpy array
+                    values = np.array(self.intensities[channel][i])
+
+                    low = np.percentile(values, 10)
+                    high = np.percentile(values, 90)
+                    mid_80 = values[(values > low) & (values < high)]
+                    # The reason convert numpy array to python list is make sure the function "cleareverything"(.clear()method) in interface moduel works fine
+                    self.intensities[channel][i] = mid_80.tolist()
+
+        self.mean_analysis()
+
+    # function to analysis mean intensities before calculating contrast
     def mean_analysis(self):
-        for i in range(0,2):
+        for i in range(0, 2):
             self.mean_red.append(np.mean(self.intensities["red"][i]))
             self.mean_blue.append(np.mean(self.intensities["blue"][i]))
             self.mean_green.append(np.mean(self.intensities["green"][i]))
+
         self.contrast_calculation()
 
     def contrast_calculation(self):
         self.red_contrast = (self.mean_red[0] - self.mean_red[1]) / self.mean_red[1]
         self.blue_contrast = (self.mean_blue[0] - self.mean_blue[1]) / self.mean_blue[1]
         self.green_contrast = (self.mean_green[0] - self.mean_green[1]) / self.mean_green[1]
-
